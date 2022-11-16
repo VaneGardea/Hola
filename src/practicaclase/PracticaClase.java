a76c820ac6a0385eb9144e433417955642e091cf
invitado <invitado@LAB010105.itchii.lab>
2022-11-16 13:46:14 -0700

    Hello

--- src/practicaclase/PracticaClase.java
+++ src/practicaclase/PracticaClase.java
@@ -16,7 +16,7 @@ public class PracticaClase {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Rocío Vanesa Gardea Hernández");
     }
     
 }
