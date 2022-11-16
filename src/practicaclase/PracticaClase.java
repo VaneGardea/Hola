c54aca222393193d50afca5af817cbb61cd0ac73
invitado <invitado@LAB010105.itchii.lab>
2022-11-16 13:44:29 -0700

    Hola

---
+++ src/practicaclase/PracticaClase.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practicaclase;
+
+/**
+ *
+ * @author invitado
+ */
+public class PracticaClase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
